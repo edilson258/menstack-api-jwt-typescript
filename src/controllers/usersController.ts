67e771fcdc33260b0fba7ee7da6a85786a206c09
edilson258 <developeredilson@gmail.com>
2022-07-22 10:10:50 +0200

    🚶‍♂️🚧 updated user controller

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -5,29 +5,41 @@ import User from '../schemas/user'
 
 export default class UserController {
   
+  // SignUp
   public async addUser(req: Request, res: Response): Promise<Response> {
-
     const { username, password } = req.body
     if(!username || !password) {
       return res.status(400).json({ error: 'username and password are rquired' })
     }
-
     if(await User.findOne({ username: username })) {
       return res.status(400).json({ error: 'username already exists' })
     }
-
     const user = await User.create({
       username: username,
       password: await bcryptjs.hash(password, await bcryptjs.genSalt())
     })
-    
-    const token = jwt.sign(user._id, process.env.JWT_SECRET || 'default', { expiresIn: '30d' }, (err, token) => {
-      if(err) {
-        console.error(err)
-        return null
-      }
-      return token
-    }) 
+    const token = signToken(user._id.toString()) 
+    if(token === null) {
+      return res.status(500).json({ error: 'Internal server error' })
+    }
     return res.status(201).json({ token })
   }
+
+  // Login
+  public async getToken(req: Request, res: Response): Promise<Response> {
+    const { username, password } = req.body
+    const user = await User.findOne({ username: username })
+    if(!user) {
+      return res.status(404).json({ error: 'User not found' })
+    }
+    if(!await bcryptjs.compare(password, user.password)) {
+      return res.status(403).json({ error: 'Wrong credentials' })
+    }
+    const token = signToken(user._id.toString())
+    return res.status(200).json({ token })
+  }
+}
+
+function signToken(payload: string) {
+  return jwt.sign(payload, process.env.JWT_SECRET || 'default', { expiresIn: '30d' })
 }
